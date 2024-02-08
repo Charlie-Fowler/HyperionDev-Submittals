1f6f821e9156685533f6ad8910bff4e3d867d946
Charlie-Fowler <152534157+Charlie-Fowler@users.noreply.github.com>
2024-02-08 15:36:49 +0000

    Create Strings.py

---
+++ String Data Type/Strings.py
@@ -0,0 +1,5 @@
+#Declare Variable "$$$Superman$$$"
+#Strip() $ when printing
+
+hero_name = "$$$Superman$$$" #stored variable as required
+print(hero_name.strip('$')) #print variable without $ symbol
