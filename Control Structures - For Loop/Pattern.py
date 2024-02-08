d806882264ddbeb2c169281c7d1f9a68bd6b020a
Charlie-Fowler <152534157+Charlie-Fowler@users.noreply.github.com>
2024-02-08 15:41:40 +0000

    Create Pattern.py

---
+++ Control Structures - For Loop/Pattern.py
@@ -0,0 +1,17 @@
+'''
+star = 0
+for x in range of 1-10
+if x is greater than than 5 subtract 1 from star variable
+else
+add 1 to star variable
+print for each interation of for
+
+
+'''
+star = 0 
+for x in range(1, 10):
+    if x > 5:
+        star -= 1
+    else:
+        star += 1
+    print('*' * star)
