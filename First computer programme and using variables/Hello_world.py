625958b9a116635410645851b63dbeefbdc32785
Charlie-Fowler <152534157+Charlie-Fowler@users.noreply.github.com>
2024-02-08 15:31:56 +0000

    Create Hello_world.py

---
+++ First computer programme and using variables/Hello_world.py
@@ -0,0 +1,8 @@
+#Request input from user requesting their name and age
+#Print users name and age as inputted
+#print "Hello World!"
+
+userName = str(input("What is your name? "))
+userAge = int(input("What is your age? "))
+print("Hello {}! You are {} years old!".format(userName,userAge))
+print("Hello World!")
